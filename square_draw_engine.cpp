317c80e70ccbf662d49a802f87f627380c30e9c5
abd <abd.manian@gmail.com>
2022-01-31 23:27:33 +0330

    Define a new header/cpp for drawing engine

---
+++ square_draw_engine.cpp
@@ -0,0 +1,29 @@
+#include "square_draw_engine.h"
+
+void sd_horizontal_line(int x, int y, int size, int space, int num_sq, Color color)
+{
+
+	int cnt {0};
+
+	while (cnt != num_sq)
+	{
+	    DrawRectangle(x, y, size, size, color);
+
+	    x += size + space;
+		cnt = cnt + 1;
+	}
+
+}
+
+void sd_vertical_line(int x, int y, int size, int space, int num_sq, Color color)
+{
+	int cnt {0};
+
+	while (cnt != num_sq)
+	{
+	    DrawRectangle(x, y, size, size, color);
+
+	    y += size + space;
+		cnt = cnt + 1;
+	}
+}
---
+++ square_draw_engine.h
@@ -0,0 +1,11 @@
+#ifndef SQUARE_DRAW_ENGINE_H_
+#define SQUARE_DRAW_ENGINE_H_
+
+#include "raylib.h"
+
+void sd_horizontal_line(int x, int y, int size, int space, int num_sq, Color color);
+
+void sd_vertical_line(int x, int y, int size, int space, int num_sq, Color color);
+
+
+#endif /* SQUARE_DRAW_ENGINE_H_ */
